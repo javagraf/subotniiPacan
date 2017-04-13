2ed68c96ff98fd6f56771e6c56061c22b3b966d2
javagraf <javagnom@gmail.com>
2017-04-13 14:53:46 +0300

    Update calcA.sql

--- calcA.sql
+++ calcA.sql
@@ -1,3 +1,3 @@
   SELECT ID, C_NAME, (CHAR_LENGTH(sentences.C_NAME)-
-            CHAR_LENGTH(Replace(sentences.C_NAME,'à','')))          
+            CHAR_LENGTH(Replace(sentences.C_NAME,'Ð°','')))          
   FROM sentences
