8a48737ad49093028885d35c7838b4e22a421a19
javagraf <javagnom@gmail.com>
2017-05-28 19:00:08 +0300

    Update otchet.sql

--- otchet.sql
+++ otchet.sql
@@ -20,3 +20,16 @@ group by pt.C_PARTNERS_NAME');
 PREPARE stmt FROM @sql;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
+
+select pt.partner_name,
+  count(case when pd.product_name = 'Product A' THEN 1 END) ProductA,
+  count(case when pd.product_name = 'Product B' THEN 1 END) ProductB,
+  count(case when pd.product_name = 'Product C' THEN 1 END) ProductC,
+  count(case when pd.product_name = 'Product D' THEN 1 END) ProductD,
+  count(case when pd.product_name = 'Product E' THEN 1 END) ProductE
+from partners pt
+left join sales s
+  on pt.part_id = s.partner_id
+left join products pd
+  on s.product_id = pd.prod_id
+group by pt.partner_name
